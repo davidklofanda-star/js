25a0b87a77fde74352d713728494d04c8ca340b2
davidklofanda-star <davidklofanda@gmail.com>
2025-12-06 21:22:17 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,37 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  typescript: { ignoreBuildErrors: true },
-  images: { unoptimized: true },
-  devIndicators: false,
-  output: "export",
-  allowedDevOrigins: [
-    "*.macaly.dev",
-    "*.macaly.app",
-    "*.macaly-app.com",
-    "*.macaly-user-data.dev",
-  ],
-  turbopack: {
-    rules: {
-      "*.{jsx,tsx}": {
-        condition: {
-          all: [
-            { not: "foreign" },
-            "development",
-          ],
-        },
-        loaders: [
-          {
-            loader: "macaly-tagger",
-            options: {
-              disableSourceMaps: true,
-            },
-          },
-        ],
-        as: "*",
-      },
-    },
-  },
+reactStrictMode: false,
+typescript: { ignoreBuildErrors: true },
+images: { unoptimized: true },
+devIndicators: false,
+output: "export",
+allowedDevOrigins: [
+"*.macaly.dev",
+"*.macaly.app",
+"*.macaly-app.com",
+"*.macaly-user-data.dev",
+],
+turbopack: {
+rules: {
+"*.{jsx,tsx}": {
+condition: {
+all: [
+{ not: "foreign" },
+"development",
+],
+},
+loaders: [
+{
+loader: "macaly-tagger",
+options: {
+disableSourceMaps: true,
+},
+},
+],
+as: "*",
+},
+},
+},
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+
